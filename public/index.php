5e32b6497c478ba1f89ca1f37429018167921505
Christophe Soares <soareschristophe@gmail.com>
2018-12-09 11:13:03 +0000

    beautify code

--- public/index.php
+++ public/index.php
@@ -5,16 +5,6 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 
-// connect to mysql atabase
-/*
-$dsn = 'mysql:dbname=pictureExample;host=127.0.0.1;charset=utf8';
-try {
-$dbh = new PDO($dsn, 'root', '12345678');
-} catch (PDOException $e) {
-die('Connection failed: ');
-}
- */
-
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver' => 'pdo_mysql',
@@ -36,31 +26,25 @@ $app->get('/', function () use ($app) {
 
 $app->get('/upload', function () use ($app) {
     return $app['twig']->render('upload_form.html.twig');
-})
-    ->bind('upload_form');
+})->bind('upload_form');
 
 $app->post('/send', function (Request $request) use ($app) {
     $file_bag = $request->files;
     $image = $file_bag->get('image');
     if (!empty($image) && $image->isValid()) {
         $data = file_get_contents($image);
-       
         $app['db']->insert('ae_gallery', array('data'=>$data));
-
     }
 
     return new RedirectResponse($app['url_generator']->generate('gallery'));
-})
-    ->bind('upload_post');
+})->bind('upload_post');
 
 $app->get('/view', function () use ($app) {
-   
     $sql = "SELECT TO_BASE64(data) as data, id, image_time FROM ae_gallery";
     $images = $app['db']->fetchAll($sql, array());
 
     return $app['twig']->render('gallery.html.twig', array('images' => $images));
-})
-    ->bind('gallery');
+})->bind('gallery');
 
 $app['debug'] = true;
 $app->run();
